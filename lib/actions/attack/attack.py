51597ce7d9b75436827e81c0a80e9fb04769ed04
natan.rauber <natanrauber9911@gmail.com>
2024-04-21 00:27:37 -0300

    Update attack.py

--- lib/actions/attack/attack.py
+++ lib/actions/attack/attack.py
@@ -77,17 +77,19 @@ def hasTarget():
 
 
 def isAttacking():
-    if type(_battle_window) is Box:
-        if (datetime.now() - _attack_start_time).seconds > 30:
-            Keyboard.press(STOP_ALL_ACTIONS_KEY)
-            return False
-        for i in range(int(_battle_window.height / 20)):
-            _pixel = pyautogui.pixel(
-                int(_battle_window.left + 23), int(_battle_window.top + (i * 20))
-            )
-            _is_attacking = _pixel[0] > 250
-            if _is_attacking:
-                return True
+    for i in range(2):
+        if type(_battle_window) is Box:
+            if (datetime.now() - _attack_start_time).seconds > 30:
+                Keyboard.press(STOP_ALL_ACTIONS_KEY)
+                return False
+            for i in range(int(_battle_window.height / 20)):
+                _pixel = pyautogui.pixel(
+                    int(_battle_window.left + 23), int(_battle_window.top + (i * 20))
+                )
+                _is_attacking = _pixel[0] > 250
+                if _is_attacking:
+                    return True
+            time.sleep(0.1)
     return False
 
 
