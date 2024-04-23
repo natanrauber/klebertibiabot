e4081856c7157ef99e47f49b60e0f01bff848979
natan.rauber <natanrauber9911@gmail.com>
2024-04-23 13:29:02 -0300

    Update setup.py

--- lib/setup.py
+++ lib/setup.py
@@ -1,7 +1,10 @@
+import pyautogui
+
 from lib.config import *
 from lib.uid import uid
 from lib.utils.console import Console
 from lib.utils.folder_manager import FolderManager
+from lib.utils.keyboard import Keyboard
 from lib.utils.window_manager import WindowManager
 
 
@@ -11,6 +14,8 @@ def setup() -> None:
 
     if not os.path.exists(TEMP_DIR):
         os.makedirs(TEMP_DIR)
+
+    pyautogui.hotkey("winleft", "m")
 
     FolderManager.clear_folder(SESSION_DIR)
     FolderManager.open_folder(SESSION_DIR)
