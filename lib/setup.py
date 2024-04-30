71d06a88c2b90eb9c19d4d5c6939ad2411554db6
natan.rauber <natanrauber9911@gmail.com>
2024-04-29 22:23:15 -0300

    Update setup.py

--- lib/setup.py
+++ lib/setup.py
@@ -1,26 +1,26 @@
+import os
+
 import pyautogui
 
-from lib.config import *
+import lib.config as cfg
 from lib.uid import uid
-from lib.utils.console import Console
 from lib.utils.folder_manager import FolderManager
-from lib.utils.keyboard import Keyboard
 from lib.utils.window_manager import WindowManager
 
 
 def setup() -> None:
-    if not os.path.exists(SESSION_DIR):
-        os.makedirs(SESSION_DIR)
+    if not os.path.exists(cfg.SESSION_DIR):
+        os.makedirs(cfg.SESSION_DIR)
 
-    if not os.path.exists(TEMP_DIR):
-        os.makedirs(TEMP_DIR)
+    if not os.path.exists(cfg.TEMP_DIR):
+        os.makedirs(cfg.TEMP_DIR)
 
     pyautogui.hotkey("winleft", "m")
 
-    FolderManager.clear_folder(SESSION_DIR)
-    FolderManager.open_folder(SESSION_DIR)
+    FolderManager.clear_folder(cfg.SESSION_DIR)
+    FolderManager.open_folder(cfg.SESSION_DIR)
     WindowManager.activate("Tibia -")
-    if not getOTServer():
+    if not cfg.getOTServer():
         WindowManager.activate("Projector")
     WindowManager.activate("session")
     WindowManager.activate(uid)
