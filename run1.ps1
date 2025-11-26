5b3513f0f52c9fb893d6fa5ab82322f4ae38a447
natan.rauber <natanrauber9911@gmail.com>
2025-11-26 07:49:24 -0300

    Update run1.ps1

--- run1.ps1
+++ run1.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 
 # Define the GitHub repository URL and the branch or release tag
 $GitHubRepoURL = "https://github.com/natanrauber/klebertibiabot"
-$BranchOrTag = "pagodera" # or whatever branch or tag you want to download
+$BranchOrTag = "master-windows" # or whatever branch or tag you want to download
 
 # Define the temporary folder path
 $TempFolder = "$env:TEMP\kleber"
